2f79c2bd52d74551701a366c57f1a5ba3d448e75
Miguel <migueltenant@gmail.com>
2019-12-15 16:29:11 -0800

    First setup NextJS

--- dep/src/components/ads.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Ads (props) {
-  return (
-    <p>Insert ads here</p>
-  );
-}
--- dep/src/components/error.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Error (props) {
-  return (
-    <p>Error: {props.message}</p>
-  );
-}
